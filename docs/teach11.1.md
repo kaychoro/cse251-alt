4e86644aaee1291b26b3a5a2c1c0a2a2bce598d4
Aaron Kay <kaya@byui.edu>
2024-07-03 11:24:10 -0600

    Added Team assignment

---
+++ docs/teach11.1.md
@@ -0,0 +1,26 @@
+![](../site/banner.png)
+
+# 11 Teach: C# Threads
+
+## Overview
+
+You will be creating a C# program to search for prime numbers in an array
+
+## Assignment
+
+Note: this is a team activity, you should take turns coding each week.  If a team member already knows how to implement the activity, they should let another team member do the coding.
+
+### Clone the template repository
+
+1. If you clone this template repository, you can open up the project under `week11/team/team11-1.csproj`
+
+## Sample Solution
+
+No solution provided.
+
+## Submission
+
+When complete, please report your progress in the associated I-Learn quiz.
+
+If you decided to do additional work on the program after your team activity, either by yourself or with others, feel free to include that additional work when you report on your progress in I-Learn.
+
---
+++ week11/team/Program.cs
@@ -0,0 +1,71 @@
+﻿/************************************
+Course: cse 251
+File: team11-1
+Week: week 11 - team activity 1
+
+Instructions:
+
+- Main contains an array of 10,000,000 random values.  You will be
+  creating threads to process this array.  If you find a prime
+  number, display it to the console.
+
+- DON'T copy/slice the array in Main() for each thread.
+
+Part 1:
+- Create a class that is a subclass of Thread.
+- create 4 threads based on this class you created.
+- Divide the array among the threads.
+
+Part 2:
+- Create a class on an interface or Runnable
+- create 4 threads based on this class you created.
+- Divide the array among the threads.
+
+Part 3:
+- Modify part1 or part 2 to handle any size array and any number
+  of threads.
+
+************************************/
+
+public static class Program {
+
+    static bool IsPrime(int n) 
+    { 
+        // Corner cases 
+        if (n <= 1) return false; 
+        if (n <= 3) return true; 
+    
+        // This is checked so that we can skip  
+        // middle five numbers in below loop 
+        if (n % 2 == 0 || n % 3 == 0) return false; 
+    
+        for (int i = 5; i * i <= n; i = i + 6) 
+            if (n % i == 0 || n % (i + 2) == 0) 
+                return false; 
+    
+        return true; 
+    }
+
+    public static void Main(String[] args) {
+        Console.WriteLine("Hello world!");
+
+        // create instance of Random class 
+        Random rand = new Random(); 
+
+        int count = 10_000_000;
+        int[] array = new int[count];
+        for (int i = 0; i < count; i++) 
+        {
+            array[i] = rand.Next();
+        }
+
+        // TODO - this is just sample code. you can remove it.
+        for (int i = 0; i < count; i++)
+        {
+            if (IsPrime(array[i]))
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
